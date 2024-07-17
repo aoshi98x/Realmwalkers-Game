d8bffa3f88b8b83dfceac9a4788e5f743b6a5133
Miguel Capibara <miguelx98@hotmail.com>
2024-07-16 22:32:48 -0500

    Correción fallo build e integración de debug console

--- Assets/Scripts/Invetory/ItemSlot.cs
+++ Assets/Scripts/Invetory/ItemSlot.cs
@@ -4,7 +4,6 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using TMPro;
 public class ItemSlot : MonoBehaviour, IPointerClickHandler
 {
     //=======ITEM DATA======//
